f922ab677aac034fd9f9f401e78e11bd590a12d2
kyle hamilton <kyleih@optonline.net>
2014-12-28 18:26:53 -0500

    stage 1 done

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,120 +1,157 @@
 'use strict';
 
 portal
-  .controller('HomeCtrl', function ($scope, $rootScope, $location, $routeParams, $filter, YahooService, history) {
-   	$scope.ys = YahooService;
-   	var _graphData = [];
-   
-   
-    _log("YahooService.indeces", YahooService.indeces)
+  .controller('HomeCtrl', function($scope, $rootScope, $location, $routeParams, $filter, YahooService, history) {
+    $scope.ys = YahooService;
 
+    //_log("YahooService.indeces", YahooService.indeces)
 
-
-    // for(var i = 0; i < YahooService.indeces.length; i++){
-    //   var idx = YahooService.indeces[i].data;
-    //   drawLineChart(idx);
-    // }
-
-    var chartData = YahooService.indeces.reduce(function(a, b) {
-      _log("a,b", a,b)
-      return a.concat(b);
+    var chartData = YahooService.indeces.map(function(val) {
+      return val.data[0];
     });
-
     drawLineChart(chartData)
 
-    function drawLineChart(data){
-    	nv.addGraph(function() {
-		    var chart = nv.models.lineChart()
-		                  .x(function(d) { return d[0] })
-		                  .y(function(d) { return d[1]})
-		                  //.color(d3.scale.category10().range())
-		                  //.useInteractiveGuideline(true)
-		                  ;
+    $scope.filter = function(o) {
+      $(event.currentTarget).parent('li').addClass("active").siblings("li").removeClass("active");
+      var filteredData = $.extend(true, [], chartData);
 
-		     chart.xAxis
-		        .tickFormat(function(d) {
-		            return d3.time.format('%x')(new Date(d))
-		          });
-		    // $('#chart svg').remove();
-		    // $('#chart').append('<svg style="height:500px">');
-		    d3.select('#chart svg')
-		        .datum(data)
-		        .call(chart);
+      filteredData.forEach(function(el, i) {
+        el.values = el.values.filter(function(ind) {
+          var interval = 0;
+          switch (o.interval) {
+            case "month":
+              interval = 2592000000;
+              break;
+            case "week":
+              interval = 604800000;
+              break;
+            case "day":
+              interval = 86400000;
+              break;
+            case "year":
+              interval = 31560000000;
+          }
+          var now = new Date().getTime();
+          var indTime = new Date(ind[0]).getTime();
+          return (indTime >= now - interval);
+        })
 
-		    nv.utils.windowResize(chart.update);
-		    return chart;
-	  	});
+        var endVal = el.values[0][1];
+        var startVal = el.values[el.values.length - 1][1];
+        var delta = (((endVal - startVal) / startVal) * 100).toFixed(2);
+        YahooService.indeces[i].delta = delta;
+      })
+
+      drawLineChart(filteredData)
     }
 
 
-    function drawBarChart(data){
-    	nv.addGraph(function() {
-		    var chart = nv.models.discreteBarChart()
-		      .x(function(d) { return d.label })    //Specify the data accessors.
-		      .y(function(d) { return d.value })
-		      .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
-		      .tooltips(true)        //Don't show tooltips
-		      .showValues(true)       //...instead, show the bar value right on top of each bar.
-		      .transitionDuration(350)
-		      ;
+    function drawLineChart(data) {
+      $scope.barchart = false;
+      $scope.linechart = true;
+      nv.addGraph(function() {
+        var chart = nv.models.lineChart()
+          .x(function(d) {
+            return d[0]
+          })
+          .y(function(d) {
+            return d[1]
+          })
+          .color(d3.scale.category10().range())
+          //.useInteractiveGuideline(true)
+        ;
 
-		    $('#chart svg').remove();
-		    $('#chart').append('<svg style="height:500px">');
-		    d3.select('#chart svg')
-		        .datum(data)
-		        .call(chart);
+        chart.xAxis
+          .tickFormat(function(d) {
+            return d3.time.format('%x')(new Date(d))
+          });
+        d3.select('#chart svg')
+          .datum(data)
+          .call(chart);
 
-		    nv.utils.windowResize(chart.update);
-
-		    return chart;
-		  });
+        nv.utils.windowResize(chart.update);
+        return chart;
+      });
     }
 
 
-    
+    function drawBarChart(data) {
+      $scope.barchart = true;
+      $scope.linechart = false;
 
-    $scope.getQuotes = function(e){
-    	$(event.currentTarget).parent('li').addClass("active").siblings("li").removeClass("active");
+      nv.addGraph(function() {
+        var chart = nv.models.discreteBarChart()
+          .x(function(d) {
+            return d.label
+          }) //Specify the data accessors.
+          .y(function(d) {
+            return d.value
+          })
+          .staggerLabels(true) //Too many bars and not enough room? Try staggering labels.
+          .tooltips(false) //Don't show tooltips
+          .showValues(true) //...instead, show the bar value right on top of each bar.
+          .transitionDuration(350);
 
-    	var symbols = YahooService.indeces.map(function(i){
-    		return i.symbol;
-    	})
-    	var options = {"symbol":symbols.join(",")}
 
-    	
-    	YahooService.getQuotes(options).then(function(res){
-    		console.log(res.data.query.results.quote)
+        d3.select('#barchart svg')
+          .datum(data)
+          .call(chart);
 
-    		var arr = res.data.query.results.quote;
+        nv.utils.windowResize(chart.update);
 
-  			YahooService.periodData["today"] = {deltas:{}, graphData:[]};
-  			YahooService.periodData["today"].graphData = [{"key": "barchart", "values": []}]
-  			arr.map(function(i){
-  				YahooService.periodData["today"].deltas[i.symbol] = i.Change;
-  				YahooService.periodData["today"].graphData[0].values.push({
-			 		"label":i.Name, "value":i.LastTradePriceOnly
-			    });
-  			})
-  			console.log(YahooService.periodData["today"].graphData)
-  			drawBarChart(YahooService.periodData["today"].graphData);
-
-    	})
-    }
-    $scope.getQuote = function(symbol, e){
-    	var options = {"symbol":symbol}
-    	YahooService.getQuotes(options).then(function(res){
-    		$scope.quote = res.data.query.results.quote;
-    		var panel = $(e.currentTarget);
-		        panel.addClass('flip open');
-		        panel.on('mouseleave', function(){
-		            $('.open').each(function(){
-		                $(this).removeClass('open flip');
-		            });
-	        	});
-	        $('.back').on('click', function(e){
-	            e.stopPropagation();
-	        });
-    	})
+        return chart;
+      });
     }
 
-});
+    $scope.showStream = function() {
+      _log("TODO")
+    }
+
+
+    $scope.getQuotes = function(e) {
+      $(event.currentTarget).parent('li').addClass("active").siblings("li").removeClass("active");
+
+      var symbols = YahooService.indeces.map(function(i) {
+        return i.symbol;
+      })
+      var options = {
+        "symbol": symbols.join(",")
+      }
+
+
+      YahooService.getQuotes(options).then(function(res) {
+        var arr = res.data.query.results.quote;
+        YahooService.today = [{
+          "key": "barchart",
+          "values": []
+        }]
+        arr.map(function(i, idx) {
+          YahooService.indeces[idx].delta = parseFloat(i.Change.replace(/[-+]/, '')).toFixed(2);
+          YahooService.today[0].values.push({
+            "label": i.Name,
+            "value": i.LastTradePriceOnly
+          });
+        })
+        drawBarChart(YahooService.today);
+      })
+    }
+    $scope.getQuote = function(symbol, e) {
+      var options = {
+        "symbol": symbol
+      }
+      YahooService.getQuotes(options).then(function(res) {
+        $scope.quote = res.data.query.results.quote;
+        var panel = $(e.currentTarget);
+        panel.addClass('flip open');
+        panel.on('mouseleave', function() {
+          $('.open').each(function() {
+            $(this).removeClass('open flip');
+          });
+        });
+        $('.back').on('click', function(e) {
+          e.stopPropagation();
+        });
+      })
+    }
+
+  });
--- js/services/yahoo.js
+++ js/services/yahoo.js
@@ -4,66 +4,76 @@ portal
   .service('YahooService', ['$http', function YahooService($http) {
     var yql = this;
 
-    yql.periodData = {
-      year:{
-        graphData : [],
-        deltas : {}
-      }
-    };
-
-
-    yql.indeces = [
-      {symbol:"^NDX",name:"NASDAQ-100",data:[],delta:0}, 
+    yql.indeces = [{
+        symbol: "^NDX",
+        name: "NASDAQ-100",
+        data: [],
+        delta: 0
+      },
       //{symbol:"^OEX",name:"S&P 100 ",data:[]},
-      {symbol:"^MID",name:"S&P MID CAP 400",data:[],delta:0},
-      {symbol:"^IXIC",name:"NASDAQ Composite",data:[],delta:0},
-      {symbol:"^XAX",name:"NYSE AMEX COMPOSITE",data:[],delta:0},
+      {
+        symbol: "^MID",
+        name: "S&P MID CAP 400",
+        data: [],
+        delta: 0
+      }, {
+        symbol: "^IXIC",
+        name: "NASDAQ Composite",
+        data: [],
+        delta: 0
+      }, {
+        symbol: "^XAX",
+        name: "NYSE AMEX COMPOSITE",
+        data: [],
+        delta: 0
+      },
       //{symbol:"^DJT",name:"Dow Jones Transportation Average",data:[]},
       //{symbol:"^DJA",name:"Dow Jones Composite Average",data:[]},
-      {symbol:"^RUT",name:"Russell 2000",data:[],delta:0}
+      {
+        symbol: "^RUT",
+        name: "Russell 2000",
+        data: [],
+        delta: 0
+      }
       //{symbol:"^RUI",name:"Russell 1000",data:[]},
       //{symbol:"^NYA",name:"NYSE COMPOSITE (DJ)",data:[]}
     ];
 
+    yql.getAllData = function(symbol) {
+      var now = new Date();
+      var yearAgo = now.setFullYear(now.getFullYear() - 1);
+      var format = d3.time.format("%Y-%m-%d");
 
-
-
-
-    yql.getAllData = function(symbol){
-        var now = new Date();
-        var yearAgo = now.setFullYear(now.getFullYear() - 1);
-        var format = d3.time.format("%Y-%m-%d");
-
-        var startDate = format(new Date(yearAgo));
-        var endDate = format(new Date());
-        var query = "select * from yahoo.finance.historicaldata where symbol = '"+symbol+"' and startDate = '"+startDate+"' and endDate = '"+endDate+"'";
-        var params = {
-          q : query,
-          format : "json",
-          diagnostics : true,
-          env : "http://datatables.org/alltables.env"
-        }
-        return $http.get('http://query.yahooapis.com/v1/public/yql', {params: params})
+      var startDate = format(new Date(yearAgo));
+      var endDate = format(new Date());
+      var query = "select * from yahoo.finance.historicaldata where symbol = '" + symbol + "' and startDate = '" + startDate + "' and endDate = '" + endDate + "'";
+      var params = {
+        q: query,
+        format: "json",
+        diagnostics: true,
+        env: "http://datatables.org/alltables.env"
+      }
+      return $http.get('http://query.yahooapis.com/v1/public/yql', {
+        params: params
+      })
     }
 
 
-
-
-    yql.getQuotes = function(options){
+    yql.getQuotes = function(options) {
       var symbol = options.symbol;
-      var query = 'select * from yahoo.finance.quote where symbol in ("'+symbol+'")';
+      var query = 'select * from yahoo.finance.quote where symbol in ("' + symbol + '")';
       var params = {
-        q : query,
-        format : "json",
-        diagnostics : true,
-        env : "http://datatables.org/alltables.env"
+        q: query,
+        format: "json",
+        diagnostics: true,
+        env: "http://datatables.org/alltables.env"
       }
-      return $http.get('http://query.yahooapis.com/v1/public/yql', {params: params})
+      return $http.get('http://query.yahooapis.com/v1/public/yql', {
+        params: params
+      })
     }
 
 
 
 
   }]);
-
-
